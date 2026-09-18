964ae848b3ee1a2fd72715d1f644b643b8703e83
pedro-limabx <phlb16092004@gmail.com>
2026-09-18 19:56:49 -0300

    feat: bootstrap CORTEX NEURON v0.1

---
+++ src/tools/registry.ts
@@ -0,0 +1,18 @@
+import type { ToolDefinition } from "../domain/types.js";
+
+export class ToolRegistry {
+  private readonly tools = new Map<string, ToolDefinition>();
+
+  register(tool: ToolDefinition): void {
+    if (this.tools.has(tool.name)) throw new Error(`Tool already registered: ${tool.name}`);
+    this.tools.set(tool.name, tool);
+  }
+
+  get(name: string): ToolDefinition | undefined {
+    return this.tools.get(name);
+  }
+
+  list(): ToolDefinition[] {
+    return [...this.tools.values()];
+  }
+}
